5e80da78d0c7ee476af57ee22c154c1ac37a9d6a
minji856 <sistar96@gmail.com>
2024-12-17 15:56:02 +0900

    fix : 불필요한 주석 제거 및 폼로그인 커스터마이징

--- src/main/java/com/minji/hi_erp/security/config/SecurityConfig.java
+++ src/main/java/com/minji/hi_erp/security/config/SecurityConfig.java
@@ -23,39 +23,35 @@ public class SecurityConfig {
     // private final UserDetailsService userDetailsService;
     private final ObjectMapper objectMapper;
 
-    // password 암호화를 위해 클래스 생성 후 빈에 등록
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        // RESTapi 이용으로 csrf(cross site request forgery)는 비활성화
-        http    .csrf(AbstractHttpConfigurer::disable)
-                .httpBasic(AbstractHttpConfigurer::disable)
-                .headers(
-                        headersConfigurer ->
-                                headersConfigurer
-                                        .frameOptions(
-                                                HeadersConfigurer.FrameOptionsConfig::sameOrigin
-                                        )
+        http    .csrf(AbstractHttpConfigurer::disable) // RESTapi 이용으로 csrf(cross site request forgery)는 비활성화
+                .httpBasic(AbstractHttpConfigurer::disable) // HTTP basic 비활성화
+                // h2-console 접근 시 localhost 거부로 인하여 xFrame 옵션 변경
+                .headers( headersConfigurer -> headersConfigurer
+                        .frameOptions( HeadersConfigurer.FrameOptionsConfig::sameOrigin
+                        )
                 )
-                /* 폼 로그인 */
-                .formLogin(form -> form
-                        .defaultSuccessUrl("/",true)
-                        .permitAll())
                 .authorizeHttpRequests(authorize -> authorize
                         // requestMatchers 에 지정된 Url 들은 인증,인가 없이도 가능
                         // 테스트를 위해 h2-console/** 추가함 (배포시 제거)
-                        .requestMatchers("/signUp","/","/login","/h2-console/**")
+                        .requestMatchers("/signUp","/","/login","/account/join","/h2-console/**")
                         .permitAll()
                         .anyRequest().authenticated())
-//                        // antMatchers 는 이제 안 쓰이는 듯
-//                        //.requestMatchers("/user").hasRole("USER") // ROLE_USER 를 가진 사용자만 접근 가능
-//                        .formLogin(form -> form.loginPage("/account/login")
-//                                .loginProcessingUrl("/login-process") // 인증처리 수행 필터 실행
-//                                .defaultSuccessUrl("/main", true) // 정상적 인증 처리 후 이동하는 페이지
-//                                .permitAll()
-//                        )
+                //.requestMatchers("/user").hasRole("USER") // ROLE_USER 를 가진 사용자만 접근 가능
+
+                /* 폼 로그인 처리 */
+                .formLogin(form -> form.loginPage("/account/login")
+                        .loginProcessingUrl("/login-process") // 인증처리 수행 필터 실행
+                        .defaultSuccessUrl("/main", true) // 정상적 인증 처리 후 이동하는 페이지
+                        .permitAll())
+
+                /* 폼 로그아웃 처리 */
                 .logout(logout -> logout
                         .logoutSuccessUrl("/login") // 로그아웃 성공 후 이동할 URL
+                        .permitAll()
                         .invalidateHttpSession(true)) // 세션 무효화
+
                 // 세션 정책
                 .sessionManagement(session -> session
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
