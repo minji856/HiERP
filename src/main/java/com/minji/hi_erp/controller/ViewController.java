c1924dba4e936f92c93d84d5708a0022d9ca2376
minji856 <sistar96@gmail.com>
2024-12-20 16:22:02 +0900

    feat : @Secured 어노테이션 추가

--- src/main/java/com/minji/hi_erp/controller/ViewController.java
+++ src/main/java/com/minji/hi_erp/controller/ViewController.java
@@ -1,14 +1,24 @@
 package com.minji.hi_erp.controller;
 
+import com.minji.hi_erp.security.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 @RequestMapping("/account")
 public class ViewController {
+
+    // private final UserService userService;
+
+//    @Autowired
+//    public ViewController(UserService userService) {
+//        this.userService = userService;
+//    }
+
     /**
      * 로그인 페이지를 표시합니다.
      *
@@ -27,5 +37,11 @@ public class ViewController {
     @GetMapping("/join")
     public String joinPage() {
         return "account/join";
+    }
+
+    @Secured("ROLE_ADMIN")        // 이 메소드에 대해서만 특정 권한이 필요할 때 사용 가능
+    @GetMapping("/info")
+    public String info(){
+        return "개인정보";
     }
 }
