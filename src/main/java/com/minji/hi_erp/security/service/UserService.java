b6b859b7fd81c8553b0df3cd053972acdd445d19
minji856 <sistar96@gmail.com>
2024-12-23 15:12:16 +0900

    fix : UserRepository 생성자 주입으로 변경하고 final 추가

--- src/main/java/com/minji/hi_erp/security/service/UserService.java
+++ src/main/java/com/minji/hi_erp/security/service/UserService.java
@@ -1,14 +1,28 @@
 package com.minji.hi_erp.security.service;
 
+import com.minji.hi_erp.security.entity.Users;
 import com.minji.hi_erp.security.repository.UserRepository;
-import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 /**
  * Spring Security에서 사용하는 UserDetailsService를 구현한 클래스입니다.
  */
 @Service
-@RequiredArgsConstructor
 public class UserService {
-    private UserRepository userRepository;
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public Users saveUsers(Users users) {
+        return userRepository.save(users);
+    }
+
+    public void deleteUsers(Long id) {
+        userRepository.deleteById(id);
+    }
 }
