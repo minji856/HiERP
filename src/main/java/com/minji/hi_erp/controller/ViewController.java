4cb6d1f7693fb4a5f31defcae43bfe7dd1461887
minji856 <sistar96@gmail.com>
2025-01-03 22:00:47 +0900

    feat : PreAuthorize 어노테이션 주석으로 추가

--- src/main/java/com/minji/hi_erp/controller/ViewController.java
+++ src/main/java/com/minji/hi_erp/controller/ViewController.java
@@ -5,6 +5,7 @@ import com.minji.hi_erp.security.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -50,6 +51,7 @@ public class ViewController {
         return "redirect:/account/join-success";
     }
 
+    // @PreAuthorize("ROLE_USER")
     @GetMapping("/join-success")
     public String joinSuccessPage() {
         System.out.println("회원가입 redirect");
