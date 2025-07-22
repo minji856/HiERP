e7a3c9810fd5747f8a66a5dbe5eadfab0d9ae278
minji856 <sistar96@gmail.com>
2025-07-23 00:24:41 +0900

    docs : UserService 클래스 설명 변경
    UserDetailsService를 구현한 클래스가 아님

--- src/main/java/com/minji/hi_erp/security/service/UserService.java
+++ src/main/java/com/minji/hi_erp/security/service/UserService.java
@@ -7,7 +7,8 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 /**
- * Spring Security에서 사용하는 UserDetailsService를 구현한 클래스입니다.
+ * 사용자 등록 및 삭제 등 사용자 관련 기능을 담당하는 서비스 클래스입니다.
+ * UserRepository를 통해 데이터베이스에 접근하며, 비밀번호 암호화 등의 비즈니스 로직을 수행합니다.
  */
 @Service
 public class UserService {
