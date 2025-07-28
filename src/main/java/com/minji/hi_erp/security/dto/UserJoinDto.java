fd1046543f54f59b6feffb93c6b0e303560daa7f
minji856 <sistar96@gmail.com>
2025-07-28 23:40:47 +0900

    feat : UserJoinDto 에도 Role 필드 추가

--- src/main/java/com/minji/hi_erp/security/dto/UserJoinDto.java
+++ src/main/java/com/minji/hi_erp/security/dto/UserJoinDto.java
@@ -1,6 +1,7 @@
 package com.minji.hi_erp.security.dto;
 
 import com.minji.hi_erp.Role;
+import com.minji.hi_erp.security.entity.Users;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -15,4 +16,11 @@ public class UserJoinDto {
     private String password;
     private String phoneNum;
     private String imageUrl;
+    private Role role;
+
+    /*
+    public Users toEntity(){
+        return Users.builder().name(name).password(password).email(email).build();
+    }
+    */
 }
