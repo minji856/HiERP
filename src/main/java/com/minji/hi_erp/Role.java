4a73dc7bc491457960276bb3634119679df9f3cf
minji856 <sistar96@gmail.com>
2025-08-28 23:37:37 +0900

    feat : Role 클래스에 title 필드 추가

--- src/main/java/com/minji/hi_erp/Role.java
+++ src/main/java/com/minji/hi_erp/Role.java
@@ -3,13 +3,19 @@ package com.minji.hi_erp;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
+/**
+ * 권한(Role)을 나타내는 열거형(Enum) 클래스입니다.
+ * 각 권한에 대한 정보를 가지고 있으며, 롬복의 @Getter와 @RequiredArgsConstructor를 사용하여
+ * Getter 메서드 및 인자를 갖는 생성자를 자동으로 생성합니다.
+ */
 @Getter
 @RequiredArgsConstructor
 public enum Role {
 
-    USER("유저"),
-    ADMIN("관리자");
+    USER("ROLE_USER","유저"),
+    ADMIN("ROLE_ADMIN","관리자");
 
     // BANNED("ROLE_BANNED", "BANNED"), USER("ROLE_USER", "USER"), ADMIN("ROLE_ADMIN", "ADMIN"), MASTER("ROLE_MASTER", "MASTER");
+    private final String title;
     private final String key;
 }
