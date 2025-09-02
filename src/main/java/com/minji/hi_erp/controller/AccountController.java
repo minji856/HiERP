a74ec74c7a1ac7ce683094a7786d6d6b802fa15f
minji856 <sistar96@gmail.com>
2025-09-03 00:02:03 +0900

    refactor : UserService에서 비밀번호 암호화하고 있어 Account컨트롤러에선 필드 삭제

--- src/main/java/com/minji/hi_erp/controller/AccountController.java
+++ src/main/java/com/minji/hi_erp/controller/AccountController.java
@@ -1,16 +1,11 @@
 package com.minji.hi_erp.controller;
 
 import com.minji.hi_erp.security.dto.UserJoinDto;
-import com.minji.hi_erp.security.dto.UserLoginDto;
 import com.minji.hi_erp.security.entity.Users;
 import com.minji.hi_erp.security.service.UserService;
-import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.annotation.Secured;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -25,7 +20,6 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 public class AccountController {
 
     private final UserService userService;
-    private final PasswordEncoder passwordEncoder;
 
     /**
      * 회원가입 페이지를 표시합니다.
