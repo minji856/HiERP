8218acea882341223335da7adff55074657e4428
minji856 <sistar96@gmail.com>
2025-09-04 03:26:50 +0900

    refactor : ViewController 어노테이션 RestController로 변경 UserService 필드 삭제

--- src/main/java/com/minji/hi_erp/controller/ViewController.java
+++ src/main/java/com/minji/hi_erp/controller/ViewController.java
@@ -1,28 +1,9 @@
 package com.minji.hi_erp.controller;
 
-import com.minji.hi_erp.security.entity.Users;
-import com.minji.hi_erp.security.service.UserService;
-import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.access.annotation.Secured;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.core.Authentication;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+@RestController
 public class ViewController {
-
-    private final UserService userService;
-
-    @Autowired
-    public ViewController(UserService userService) {
-        this.userService = userService;
-    }
 
     /**
      * Hi-E 메인 페이지를 표시합니다.
