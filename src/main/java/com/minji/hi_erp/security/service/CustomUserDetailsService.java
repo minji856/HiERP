bc63bedfca162fefc09c183addc232bd8d00d1cf
minji856 <sistar96@gmail.com>
2025-09-05 17:39:58 +0900

    docs : loadUserByUsername 에 대한 설명 주석화
    실제론 email을 로딩함

--- src/main/java/com/minji/hi_erp/security/service/CustomUserDetailsService.java
+++ src/main/java/com/minji/hi_erp/security/service/CustomUserDetailsService.java
@@ -14,11 +14,21 @@ public class CustomUserDetailsService implements UserDetailsService {
 
     private final UserRepository userRepository;
 
+    /**
+     * 유저를 이메일로 불러오는 메서드입니다.
+     * @param email
+     * @return
+     * @throws UsernameNotFoundException
+     */
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
         Users users = userRepository.findByEmail(email)
                 .orElseThrow(() -> new UsernameNotFoundException("이메일이 존재하지 않습니다.: " + email));
 
         return new CustomUserDetails(users);
+
+//        return userRepository.findByEmail(email)
+//                .map(user -> new CustomUserDetails(user))
+//                .orElseThrow(() -> new UsernameNotFoundException("사용자를 찾을 수 없습니다."));
     }
 }
