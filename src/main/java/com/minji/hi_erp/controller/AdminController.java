51f4eaa81507ca18fb9f0ba823c9c42d98665287
minji856 <sistar96@gmail.com>
2025-09-12 23:00:18 +0900

    feat : 관리자권한에서 회원리스트 출력 메서드와 회원삭제 기능 추가

--- src/main/java/com/minji/hi_erp/controller/AdminController.java
+++ src/main/java/com/minji/hi_erp/controller/AdminController.java
@@ -1,11 +1,11 @@
 package com.minji.hi_erp.controller;
 
+import com.minji.hi_erp.security.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * 관리자 관련 컨트롤러 입니다.
@@ -15,14 +15,23 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @RequestMapping("/admin")
 public class AdminController{
 
-/**
- * 회원 관리 페이지 (ROLE_ADMIN 권한 필요)
- */
-@Secured("ROLE_ADMIN")
-@GetMapping("/admin_setting")
-public String adminPage() {
-    System.out.println("당신은 관리자입니다");
-    return "/admin/admin_setting";
-}
+    private final UserService userService;
+
+    /**
+     * 회원 관리 리스트 페이지 (ROLE_ADMIN 권한 필요)
+     */
+    @Secured("ROLE_ADMIN")
+    @GetMapping("/admin_setting")
+    public String adminGetUserPage(Model model) {
+        model.addAttribute("users", userService.findAll());
+        return "/admin/admin_setting";
+    }
+
+    // 회원 삭제 (form submit)
+    @PostMapping("/delete/{id}")
+    public String deleteUser(@PathVariable Long id) {
+        userService.deleteUserByAdmin(id);
+        return "redirect:/admin/admin_setting"; // 삭제 후 리스트로 리다이렉트
+    }
 
 }
