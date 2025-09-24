4cdc0d60fd7b058d4306aa69a1f154751cf7fd85
minji856 <sistar96@gmail.com>
2025-09-24 23:36:58 +0900

    feat : Fullcalendar 기본 설정 추가 하고 변수타입 let으로 변경

--- src/main/resources/static/js/calendar.js
+++ src/main/resources/static/js/calendar.js
@@ -1,7 +1,22 @@
 document.addEventListener('DOMContentLoaded', function() {
-    var calendarEl = document.getElementById('calendar');
-    var calendar = new FullCalendar.Calendar(calendarEl, {
-    initialView: 'dayGridMonth'
-});
+    let calendarEl = document.getElementById('calendar');
+
+    let calendar = new FullCalendar.Calendar(calendarEl, {
+        initialView: 'dayGridMonth',
+        headerToolbar: {
+            // customButton 은 left 또는 right 안에 넣으면 적용 된다
+            left: 'prev,next,today',  // today는 , 와 띄어쓰기에 따라서 위치가 바뀐다
+            center: 'title',
+            right: 'dayGridMonth,timeGridWeek,timeGridDay',
+        },
+        googleCalendarApiKey: '<YOUR API KEY>',
+            eventSources: [
+                {
+                    googleCalendarId: 'efgh5678@group.calendar.google.com',
+                    className: 'ko-event'
+                }
+            ],
+        locale : 'ko'
+    });
     calendar.render();
 });
