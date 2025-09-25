5dd46567c0ae9ac12615fd9bbabbe6f6b1094445
minji856 <sistar96@gmail.com>
2025-09-25 21:08:40 +0900

    feat : buttonText 한글로 변경
    locale 'ko' 설정과 별개로 직접 한글로 바꿔줘야함

--- src/main/resources/static/js/calendar.js
+++ src/main/resources/static/js/calendar.js
@@ -3,11 +3,18 @@ document.addEventListener('DOMContentLoaded', function() {
 
     let calendar = new FullCalendar.Calendar(calendarEl, {
         initialView: 'dayGridMonth',
+        locale : 'ko',
         headerToolbar: {
             // customButton 은 left 또는 right 안에 넣으면 적용 된다
             left: 'prev,next,today',  // today는 , 와 띄어쓰기에 따라서 위치가 바뀐다
             center: 'title',
-            right: 'dayGridMonth,timeGridWeek,timeGridDay',
+            right: 'dayGridMonth,timeGridWeek,timeGridDay'
+        },
+        buttonText: {
+            today: '오늘',
+            month: '월',
+            week: '주',
+            day: '일'
         },
         googleCalendarApiKey: '<YOUR API KEY>',
             eventSources: [
@@ -16,7 +23,6 @@ document.addEventListener('DOMContentLoaded', function() {
                     className: 'ko-event'
                 }
             ],
-        locale : 'ko'
     });
     calendar.render();
 });
