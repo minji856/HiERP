0ba259e8d1224f8efa59b533ef6da12760a694c1
minji856 <sistar96@gmail.com>
2025-09-30 23:54:45 +0900

    refactor : Calendar렌더링 부분을 fetch 형식으로 변경
    Api Key는 서버에서 관리하고 JavaScript 에서는 실행 시점에서 가져오게 보안성 고려함

--- src/main/resources/static/js/calendar.js
+++ src/main/resources/static/js/calendar.js
@@ -1,28 +1,31 @@
 document.addEventListener('DOMContentLoaded', function() {
     let calendarEl = document.getElementById('calendar');
 
-    let calendar = new FullCalendar.Calendar(calendarEl, {
-        initialView: 'dayGridMonth',
-        locale : 'ko',
-        headerToolbar: {
-            // customButton 은 left 또는 right 안에 넣으면 적용 된다
-            left: 'prev,next,today',  // today는 , 와 띄어쓰기에 따라서 위치가 바뀐다
-            center: 'title',
-            right: 'dayGridMonth,timeGridWeek,timeGridDay'
-        },
-        buttonText: {
-            today: '오늘',
-            month: '월',
-            week: '주',
-            day: '일'
-        },
-        googleCalendarApiKey: '<YOUR API KEY>',
-            eventSources: [
-                {
-                    googleCalendarId: 'ko.south_korea#holiday@group.v.calendar.google.com',
-                    className: 'ko-event'
-                }
-            ],
+    fetch('/api/calendar/key')
+        .then(response => response.text())
+        .then(apiKey => {
+            let calendar = new FullCalendar.Calendar(calendarEl, {
+                initialView: 'dayGridMonth',
+                locale: 'ko',
+                headerToolbar: {
+                    left: 'prev,next,today',
+                    center: 'title',
+                    right: 'dayGridMonth,timeGridWeek,timeGridDay'
+                },
+                buttonText: {
+                    today: '오늘',
+                    month: '월',
+                    week: '주',
+                    day: '일'
+                },
+                googleCalendarApiKey: apiKey,
+                eventSources: [
+                    {
+                        googleCalendarId: 'ko.south_korea#holiday@group.v.calendar.google.com',
+                        className: 'ko-event'
+                    }
+                ],
+            });
+        calendar.render();
     });
-    calendar.render();
 });
