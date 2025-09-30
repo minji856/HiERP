66f619c2ab94c9a09ce0e07c9311373f536538a3
minji856 <sistar96@gmail.com>
2025-10-01 00:37:43 +0900

    feat : ApiKey 생성자 주입 후 get 으로 반환

--- src/main/java/com/minji/hi_erp/controller/ApiKeyController.java
+++ src/main/java/com/minji/hi_erp/controller/ApiKeyController.java
@@ -1,11 +1,20 @@
 package com.minji.hi_erp.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ApiKeyController {
 
-    @Value("${google.api.key}")
-    private String googleApiKey;
+    private final String googleApiKey;
+
+    public ApiKeyController(@Value("${google.api.key}") String googleApiKey) {
+        this.googleApiKey = googleApiKey;
+    }
+
+    @GetMapping("/api/calendar")
+    public String getGoogleApiKey() {
+        return googleApiKey;
+    }
 }
