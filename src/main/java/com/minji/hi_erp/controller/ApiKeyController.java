67cb13c4066e85fef8bd497e32ed485efe3e5b3f
minji856 <sistar96@gmail.com>
2025-10-02 15:58:19 +0900

    feat : Prxoy 방식으로 변경
    기존 메서드는 Api 키만 반환함

--- src/main/java/com/minji/hi_erp/controller/ApiKeyController.java
+++ src/main/java/com/minji/hi_erp/controller/ApiKeyController.java
@@ -1,20 +1,31 @@
 package com.minji.hi_erp.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Map;
 
 @RestController
 public class ApiKeyController {
 
     private final String googleApiKey;
+    private final String KoHolidayCalendarId = "ko.south_korea#holiday@group.v.calendar.google.com";
 
     public ApiKeyController(@Value("${google.api.key}") String googleApiKey) {
         this.googleApiKey = googleApiKey;
     }
 
     @GetMapping("/api/calendar")
-    public String getGoogleApiKey() {
-        return googleApiKey;
+    public ResponseEntity<?> getHolidayApiKey() {
+        String url = "https://www.googleapis.com/calendar/v3/calendars/"
+                + KoHolidayCalendarId
+                + "/events?key=" + googleApiKey;
+        RestTemplate restTemplate = new RestTemplate();
+        Map response = restTemplate.getForObject(url, Map.class);
+
+        return ResponseEntity.ok(response);
     }
 }
