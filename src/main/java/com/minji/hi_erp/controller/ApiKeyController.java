f13135a3fc2bf04869b5dd7bc1de185b4da5a2d4
minji856 <sistar96@gmail.com>
2025-10-13 21:53:37 +0900

    feat : Value 어노테이션으로 GoogleApi 활용하여 공휴일 띄우기
    RestTemplate로 전체 인코딩하지않고, UriComponentsBuilder로 인코딩함.
    계속 2중으로 인코딩 되었음

--- src/main/java/com/minji/hi_erp/controller/ApiKeyController.java
+++ src/main/java/com/minji/hi_erp/controller/ApiKeyController.java
@@ -1,6 +1,7 @@
 package com.minji.hi_erp.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,7 +13,10 @@ import java.util.Map;
 public class ApiKeyController {
 
     private final String googleApiKey;
-    private final String KoHolidayCalendarId = "ko.south_korea#holiday@group.v.calendar.google.com";
+    private final RestTemplate restTemplate = new RestTemplate();
+
+    // 한국 공휴일 캘린더 ID (정확히 인코딩된 형태)
+    private static final String KO_HOLIDAY_CALENDAR_ID = "ko.south_korea#holiday@group.v.calendar.google.com";
 
     public ApiKeyController(@Value("${google.api.key}") String googleApiKey) {
         this.googleApiKey = googleApiKey;
@@ -20,12 +24,20 @@ public class ApiKeyController {
 
     @GetMapping("/api/calendar")
     public ResponseEntity<?> getHolidayApiKey() {
-        String url = "https://www.googleapis.com/calendar/v3/calendars/"
-                + KoHolidayCalendarId
-                + "/events?key=" + googleApiKey;
-        RestTemplate restTemplate = new RestTemplate();
-        Map response = restTemplate.getForObject(url, Map.class);
+        try {
+            String url = "https://www.googleapis.com/calendar/v3/calendars/{calendarId}/events?key={apiKey}";
 
-        return ResponseEntity.ok(response);
+            Map<String, String> uriVariables = Map.of(
+                    "calendarId", KO_HOLIDAY_CALENDAR_ID,
+                    "apiKey", googleApiKey
+            );
+
+            Map response = restTemplate.getForObject(url, Map.class, uriVariables);
+            return ResponseEntity.ok(response);
+
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("{\"error\": \"" + e.getMessage() + "\"}");
+        }
     }
 }
