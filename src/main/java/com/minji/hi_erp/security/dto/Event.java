9082ef31df02a7e3915940c918f10d1218071897
minji856 <sistar96@gmail.com>
2025-10-20 09:57:32 +0900

    ✨ Calendar Event DTO 생성

---
+++ src/main/java/com/minji/hi_erp/security/dto/Event.java
@@ -0,0 +1,25 @@
+package com.minji.hi_erp.security.dto;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Event {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    private String startDate;
+
+    private String endDate;
+}
