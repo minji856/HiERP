1b2519b4994061a609bcb7e01b6ee4e18abeae11
minji856 <sistar96@gmail.com>
2025-10-22 23:53:25 +0900

    ✨ EventDto startDate에 "yyyy-mm-dd" pattern 추가

--- src/main/java/com/minji/hi_erp/security/dto/EventDto.java
+++ src/main/java/com/minji/hi_erp/security/dto/EventDto.java
@@ -1,5 +1,6 @@
 package com.minji.hi_erp.security.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -7,6 +8,8 @@ import lombok.Setter;
 @Setter
 public class EventDto {
     private String title;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
     private String startDate; // yyyy-mm-dd 형식
     private String endDate;
 }
