89677927b4cc4bc74833fdbd6df1c1fdbc942c69
minji856 <sistar96@gmail.com>
2025-10-24 16:22:35 +0900

    🔥 JsonFormat 어노테이션 삭제 + 필드명 start, end 로 변경
    Fullcalendar에서 인식할 수 있도록 startDate, endDate 필드를 start, end로 변경(Entity는 그대로 startDate)

--- src/main/java/com/minji/hi_erp/security/dto/EventDto.java
+++ src/main/java/com/minji/hi_erp/security/dto/EventDto.java
@@ -1,6 +1,5 @@
 package com.minji.hi_erp.security.dto;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -8,8 +7,6 @@ import lombok.Setter;
 @Setter
 public class EventDto {
     private String title;
-
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
-    private String startDate; // yyyy-mm-dd 형식
-    private String endDate;
+    private String start;
+    private String end;
 }
