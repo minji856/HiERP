46da698fb99e6b41dd7eb828c62e2e29cbea7771
minji856 <sistar96@gmail.com>
2025-10-24 16:23:09 +0900

    ✨ Repository 어노테이션 누락으로 추가함

--- src/main/java/com/minji/hi_erp/security/repository/EventRepository.java
+++ src/main/java/com/minji/hi_erp/security/repository/EventRepository.java
@@ -2,6 +2,8 @@ package com.minji.hi_erp.security.repository;
 
 import com.minji.hi_erp.security.entity.Event;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface EventRepository extends JpaRepository<Event, Long> {
 }
