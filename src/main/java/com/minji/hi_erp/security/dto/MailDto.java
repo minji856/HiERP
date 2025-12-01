fd13d3faf5b668dc0843849a5d7e2b61197e3c01
minji856 <sistar96@gmail.com>
2025-12-01 16:07:52 +0900

    ✨ Feat : MailDto 생성 템플릿엔진 패턴 사용
    mail 경로는 templates/mail/ 로 별도로 setTemplates 메서드 설정

---
+++ src/main/java/com/minji/hi_erp/security/dto/MailDto.java
@@ -0,0 +1,27 @@
+package com.minji.hi_erp.security.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Map;
+
+/**
+ * 이메일 전송을 위한 DTO(Data Transfer Object) 클래스입니다.
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class MailDto {
+    private String sendTo; // 수신자
+    private String title; // 메일 제목
+    private String message;
+    private Map<String, Object> content; // 내용
+    private String templates; // 사용할 templates 파일 이름
+
+    // templates 경로가 /templates/mail/ 로 설정
+    public void setTemplates(String templates){
+        this.templates = "mail/"+templates;
+    }
+
+}
