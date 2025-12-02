89bdadd77d3414e5e7865849b98d90c2f464b425
minji856 <sistar96@gmail.com>
2025-12-02 21:47:51 +0900

    ✨ Feat : MimeMessage 멀티파트 사용. 템플릿 프로세스 설정
    html 형식으로 보내기 위함

--- src/main/java/com/minji/hi_erp/security/service/EmailService.java
+++ src/main/java/com/minji/hi_erp/security/service/EmailService.java
@@ -1,27 +1,46 @@
 package com.minji.hi_erp.security.service;
 
+import com.minji.hi_erp.security.dto.MailDto;
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
 import lombok.RequiredArgsConstructor;
-import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+import org.thymeleaf.context.Context;
+import org.thymeleaf.spring6.SpringTemplateEngine;
+
+import java.io.IOException;
 
 /**
  * 이메일을 보내는 서비스 입니다.
  * JavaMailSender 주입
  */
+//@Slf4j Logger 어노테이션
 @Service
 @RequiredArgsConstructor
 public class EmailService {
 
     private final JavaMailSender mailSender;
 
-    public void sendPasswordEmail(String to, String subject, String content) {
-        SimpleMailMessage message = new SimpleMailMessage();
+    private final SpringTemplateEngine templateEngine;
 
-        message.setTo(to);
-        message.setSubject(subject);
-        message.setText(content);
+    public void sendEmail(MailDto mailDto) throws MessagingException, IOException {
+        MimeMessage mimeMessage = mailSender.createMimeMessage();
+        // 멀티파트 설정
+        MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, true, "utf-8");
 
-        mailSender.send(message);
+        helper.setSubject(mailDto.getTitle()); // 메일 제목 설정
+        helper.setTo(mailDto.getSendTo()); // 수신자 설정
+
+        // 템플릿을 통해 전달할 데이터 설정
+        Context context = new Context();
+        context.setVariables(mailDto.getContext()); // 내용 설정
+
+        // 메일 내용 설정 - 템플릿 프로세스
+        String html = templateEngine.process(mailDto.getTemplates(),context);
+        helper.setText(html, true);
+
+        mailSender.send(mimeMessage);
     }
 }
