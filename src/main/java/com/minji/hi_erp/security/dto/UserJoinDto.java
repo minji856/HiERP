ef4df49069fee5e73a0bb64933801673d3fc2ea4
minji856 <sistar96@gmail.com>
2026-01-21 23:18:32 +0900

    ⚡ Refactor : UserJoinDto에 Birthday 필드 추가

--- src/main/java/com/minji/hi_erp/security/dto/UserJoinDto.java
+++ src/main/java/com/minji/hi_erp/security/dto/UserJoinDto.java
@@ -3,8 +3,13 @@ package com.minji.hi_erp.security.dto;
 import com.minji.hi_erp.Role;
 import com.minji.hi_erp.security.entity.Users;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDate;
+import java.util.Date;
 
 /**
  * 폼을 통한 회원 가입 내용을 담을 dto 클래스 입니다.
@@ -15,6 +20,10 @@ public class UserJoinDto {
 
     @NotBlank(message = "이름은 필수입니다.")
     private String name;
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    @NotNull(message = "생년원일은 필수입니다.")
+    private LocalDate birthday;
 
     @NotBlank(message = "이메일은 필수입니다.")
     private String email;
