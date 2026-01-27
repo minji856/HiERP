431f5082a33b7c5aa61e5845d0118f9f03385a73
minji856 <sistar96@gmail.com>
2026-01-27 23:11:06 +0900

    ⚡ Refactor : 계정 상태 활성화 하는 enable(), disable() 메서드 생성
    setter로 사용하지 못하게 도메인 메서드 방식 사용

--- src/main/java/com/minji/hi_erp/security/entity/Users.java
+++ src/main/java/com/minji/hi_erp/security/entity/Users.java
@@ -56,7 +56,7 @@ public class Users {
         this.imageUrl = imageUrl;
         // 회원가입 시 권한은 서버에서 기본 USER로 고정 (권한 상승/조작 방지)
         this.role = Role.USER;
-        // 회원가입 시 기본값은 false로 설정. 이메일 인증시 true 변환
+        // 회원가입 시 인증 전에는 false로 설정하여 로그인 불가. 이메일 인증시 true로 변환
         this.enabled = false;
     }
 
@@ -72,5 +72,16 @@ public class Users {
 
     public void updatePassword(String password, PasswordEncoder passwordEncoder) {
         this.password=passwordEncoder.encode(password);
+    }
+
+    // 계정상태 활성화 처리하는 메서드입니다. (이메일 인증 또는 관리자 승인 완료 시 호출)
+    public void enable() {
+        this.enabled = true;
+    }
+
+
+    // 계정상태를 비활성화 처리하는 메서드입니다.
+    public void disable() {
+        this.enabled = false;
     }
 }
