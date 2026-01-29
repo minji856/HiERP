7ada387cdb5e284c3f01d685216b80628adb94d0
minji856 <sistar96@gmail.com>
2026-01-29 18:58:56 +0900

    ✨ Feat : 1회성 인증을 위한 EmailToken 엔티티 생성
    token : unique로 지정하고 Users 엔티티와 1:1 조인

---
+++ src/main/java/com/minji/hi_erp/security/entity/EmailToken.java
@@ -0,0 +1,29 @@
+package com.minji.hi_erp.security.entity;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+/**
+ * 회원가입 시 인증용 토큰을 생성하는 엔티티 클래스입니다.
+ * Users 엔티티와 1:1 조인하여 user_id 참고
+ */
+@Entity
+public class EmailToken {
+
+    @Id @GeneratedValue
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String token;
+
+    @OneToOne
+    @JoinColumn(name = "user_id")
+    private Users user;
+
+    private LocalDateTime expiredAt;
+
+    private boolean isExpired(){
+        return LocalDateTime.now().isAfter(expiredAt);
+    }
+}
