cfa806483d0be3e1167164b00382d31f235a3538
minji856 <sistar96@gmail.com>
2026-02-18 23:34:20 +0900

    ⚡ Chore : 비밀번호 변경 필드에 정규식 추가

--- src/main/java/com/minji/hi_erp/security/dto/ChangePasswordRequestDto.java
+++ src/main/java/com/minji/hi_erp/security/dto/ChangePasswordRequestDto.java
@@ -1,5 +1,7 @@
 package com.minji.hi_erp.security.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -14,7 +16,12 @@ import lombok.Setter;
 @AllArgsConstructor
 public class ChangePasswordRequestDto {
 
+    @NotBlank
     private String exPassword; // 이전 비밀번호
+
+    @NotBlank
+    @Pattern(regexp = "(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!#$%^&*]).{8,16}",
+        message = "비밀번호는 8~16자 영문 대 소문, 숫자, 특수문자를 사용하세요.")
     private String newPassword; // 새 비밀번호
     private String newPasswordChk; // 새 비밀번호 확인
 }
