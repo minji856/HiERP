e9b03914985f530515835cae116185ba526a8011
minji856 <sistar96@gmail.com>
2026-02-24 23:32:24 +0900

    ⚡ Chore : 어노테이션 주석 추가

--- src/main/java/com/minji/hi_erp/HiErpApplication.java
+++ src/main/java/com/minji/hi_erp/HiErpApplication.java
@@ -3,12 +3,23 @@ package com.minji.hi_erp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.web.filter.HiddenHttpMethodFilter;
 
-// db 없이 실행하기 위하여 exclude 속성 추가
+// DB 없이 실행하기 위하여 exclude={DataSourceAutoConfiguration.class} 속성 추가
 // @SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
+// @EnableJpaAuditing - 생성/수정일 자동화하기 위해서 필요한 어노테이션
+@EnableJpaAuditing
 @SpringBootApplication
 public class HiErpApplication {
-	public static void main(String[] args) {
-		SpringApplication.run(HiErpApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(HiErpApplication.class, args);
+    }
+
+    // DeleteMapping, PutMapping 을 쓰기 위한 필터 등록
+//    @Bean
+//    public HiddenHttpMethodFilter hiddenHttpMethodFilter() {
+//        return new HiddenHttpMethodFilter();
+//    }
 }
