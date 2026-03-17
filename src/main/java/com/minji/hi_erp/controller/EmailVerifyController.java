3fd44611a4abbc2a252095035b15b959ec1ba6d0
minji856 <sistar96@gmail.com>
2026-03-17 23:09:44 +0900

    ✨ Feat : token값 확인해주는 EmailVerifyController 생성
    url 값의 token 값 비교

--- src/main/java/com/minji/hi_erp/controller/EmailVerifyController.java
+++ src/main/java/com/minji/hi_erp/controller/EmailVerifyController.java
@@ -1,7 +1,29 @@
 package com.minji.hi_erp.controller;
 
-import org.springframework.stereotype.Controller;
+import com.minji.hi_erp.security.service.EmailVerifyService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
+@RequiredArgsConstructor
+@Slf4j
 public class EmailVerifyController {
+
+    private final EmailVerifyService emailVerifyService;
+
+    @GetMapping("/account/verify")
+    public ResponseEntity<String> verifyAccount (@RequestParam(value = "token") String token){
+        try {
+            emailVerifyService.verifyEmail(token);
+            return ResponseEntity.ok("이메일 인증이 완료되었습니다. 로그인이 가능합니다.");
+        } catch (Exception e) {
+            log.error("이메일 인증 실패 : {}", e.getMessage());
+
+            return ResponseEntity.badRequest().body("인증 실패 :" + e.getMessage());
+        }
+    }
 }
