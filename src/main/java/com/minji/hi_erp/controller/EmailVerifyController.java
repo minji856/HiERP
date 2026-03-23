e243fdbb7045afcdda7001e93dca37f314f011d4
minji856 <sistar96@gmail.com>
2026-03-23 23:31:42 +0900

    ✨ Feat : 이메일 인증 링크시 로그인 페이지로 리다렉트
    message, error로 사용자 편의성 추가

--- src/main/java/com/minji/hi_erp/controller/EmailVerifyController.java
+++ src/main/java/com/minji/hi_erp/controller/EmailVerifyController.java
@@ -3,12 +3,12 @@ package com.minji.hi_erp.controller;
 import com.minji.hi_erp.service.EmailVerifyService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-@RestController
+@Controller
 @RequiredArgsConstructor
 @Slf4j
 public class EmailVerifyController {
@@ -16,14 +16,17 @@ public class EmailVerifyController {
     private final EmailVerifyService emailVerifyService;
 
     @GetMapping("/account/verify")
-    public ResponseEntity<String> verifyAccount (@RequestParam(value = "token") String token){
+    public String verifyAccount (@RequestParam(value = "token") String token, RedirectAttributes attributes) {
         try {
             emailVerifyService.verifyEmail(token);
-            return ResponseEntity.ok("이메일 인증이 완료되었습니다. 로그인이 가능합니다.");
+            attributes.addFlashAttribute("message","이메일 인증이 완료되었습니다. 로그인해주세요!");
+            // return ResponseEntity.ok("이메일 인증이 완료되었습니다. 로그인이 가능합니다.");
+            return "redirect:/account/login";
         } catch (Exception e) {
+            attributes.addFlashAttribute("error", "인증에 실패하였습니다." + e.getMessage());
             log.error("이메일 인증 실패 : {}", e.getMessage());
-
-            return ResponseEntity.badRequest().body("인증 실패 :" + e.getMessage());
+            return "redirect:/account/login"; // return "redirect:/login?error=true"; 추후 에러 페이지
+            // return ResponseEntity.badRequest().body("인증 실패 :" + e.getMessage());
         }
     }
 }
