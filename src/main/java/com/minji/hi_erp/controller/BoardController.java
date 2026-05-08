af671d2292cc1422fb7a6b7cf75c269e64225c7d
minji856 <sistar96@gmail.com>
2026-05-08 21:43:18 +0900

    ♻️ Refactor : "/board" 메핑 제거

--- src/main/java/com/minji/hi_erp/controller/BoardController.java
+++ src/main/java/com/minji/hi_erp/controller/BoardController.java
@@ -10,10 +10,8 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/board")
 @RequiredArgsConstructor
 public class BoardController {
 
