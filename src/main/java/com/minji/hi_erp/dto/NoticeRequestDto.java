cbd2976df61dcc4908028fec6a8acbd3634597c3
minji856 <sistar96@gmail.com>
2026-05-15 20:59:12 +0900

    ✨ Feat : NoticeRequestDto 생성

--- src/main/java/com/minji/hi_erp/dto/NoticeRequestDto.java
+++ src/main/java/com/minji/hi_erp/dto/NoticeRequestDto.java
@@ -1,4 +1,16 @@
 package com.minji.hi_erp.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * 글쓰기(save)와 수정(update)시점에 클라이언트로부터 전달받는 Dto 입니다.
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class NoticeRequestDto {
+    private String title;
+    private String content;
 }
