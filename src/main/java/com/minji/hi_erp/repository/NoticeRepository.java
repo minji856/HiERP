1341693eef29108cc9173b6640a8139876f90775
minji856 <sistar96@gmail.com>
2026-06-17 20:00:49 +0900

    ✨ Feat : 기존 레포지토리에 DB가 수정하는 로직 추가

--- src/main/java/com/minji/hi_erp/repository/NoticeRepository.java
+++ src/main/java/com/minji/hi_erp/repository/NoticeRepository.java
@@ -4,6 +4,9 @@ import com.minji.hi_erp.entity.Notice;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -18,4 +21,8 @@ public interface NoticeRepository extends JpaRepository<Notice, Long> {
 
     // 가장 최신 5개글만
     List<Notice> findTop5ByOrderByCreatedDateDesc();
+
+    @Modifying // 💡 INSERT, UPDATE, DELETE 쿼리임을 나타냄 (필수)
+    @Query("UPDATE Notice n SET n.viewCount = n.viewCount + 1 WHERE n.id = :id")
+    int updateViewCount(@Param("id") Long id);
 }
