5bfcd08e5d2b9a502d50e0b612a66dbcff15afa9
minji856 <sistar96@gmail.com>
2026-06-18 21:53:33 +0900

    Feature/cookie : 조회수 누락방지 Query 쿠키 생성
    * ♻️ Refactor : 공지사항 상세 조회와 조회수 증가 로직 분리
    
    * ✨ Feat : noticeView라는 자정까지만 유효한 쿠키 설정 로직 생성
    
    * ✅ Test : 동시성 누락 테스트 코드 작성
    
    * ✨ Feat : 기존 레포지토리에 DB가 수정하는 로직 추가
    
    * ⚡ Refactor : 조회수 증가를 Query로 활용

--- src/main/java/com/minji/hi_erp/controller/NoticeController.java
+++ src/main/java/com/minji/hi_erp/controller/NoticeController.java
@@ -6,6 +6,9 @@ import com.minji.hi_erp.entity.Notice;
 import com.minji.hi_erp.entity.Users;
 import com.minji.hi_erp.service.CustomUserDetails;
 import com.minji.hi_erp.service.NoticeService;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -20,6 +23,12 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.ZoneId;
+import java.util.Arrays;
 
 @Controller
 @RequiredArgsConstructor
@@ -63,12 +72,70 @@ public class NoticeController {
      *
      */
     @GetMapping("/{id}")
-    public String detail(@PathVariable Long id, Model model){
+    public String detail(@PathVariable Long id, HttpServletRequest  request, HttpServletResponse response, Model model){
+        // 브라우저가 보낸 쿠키들 중에서 "noticeView"라는 이름의 쿠키가 있는지 찾는다.
+        Cookie[] cookies = request.getCookies();
+        System.out.println(Arrays.toString(cookies));
+
+        // 쿠키 이름 저장을 위한 빈 객체 생성
+        Cookie oldCookie = null;
+
+        long secondsUntilMidnight = getSecondsUntilMidnight();
+
+        if (cookies != null) {
+            for (Cookie cookie : cookies){
+                if (cookie.getName().equals("noticeView")){
+                    oldCookie = cookie;
+                    break;
+                }
+            }
+        }
+
+        // noticeView 쿠키가 존재 할 때 (이미 읽었을 때)
+        if (oldCookie != null){
+            // 현재 게시글 ID가 쿠키 값에 포함되어 있지 않은지 확인합니다. (예: "[1][3][5]")
+            if (!oldCookie.getValue().contains("[" + id + "]")){
+                // 조회수 증가
+                //noticeService.increaseViewCount(id);
+                noticeService.increasViewCountatDB(id);
+                // 쿠키 값 뒤에 현재 글 ID를 이어 붙입니다. (예: "[1][3]" -> "[1][3][5]")
+                oldCookie.setValue(oldCookie.getValue() + "[" + id + "]");
+                oldCookie.setPath("/");
+                //oldCookie.setMaxAge(60 * 60 * 24);
+                oldCookie.setMaxAge((int) secondsUntilMidnight);
+                response.addCookie(oldCookie);
+            }
+        }
+
+        // 게시판을 처음 보는 사람
+        else {
+            //noticeService.increaseViewCount(id);
+            noticeService.increasViewCountatDB(id);
+            Cookie newCookie = new Cookie("noticeView", "[" + id + "]");
+            newCookie.setPath("/"); // 모든 경로에서 접근 가능
+            // newCookie.setMaxAge(60 * 60 * 24);
+            newCookie.setMaxAge((int) secondsUntilMidnight);
+            response.addCookie(newCookie);
+        }
+
         NoticeResponseDto responseDto = noticeService.findById(id);
         model.addAttribute("notice", responseDto);
 
         return "board/detail";
    }
+
+    /**
+     * 현재 시간부터 오늘 밤 12시 직전까지 남은 시간을 초(Seconds)로 계산하는 메서드
+     */
+    private long getSecondsUntilMidnight() {
+        // LocalDateTime now = LocalDateTime.now();
+        // 서버 위치와 상관없이 강제로 한국 시간대(Asia/Seoul)의 현재 시간을 가져옴
+        LocalDateTime now = LocalDateTime.now(ZoneId.of("Asia/Seoul"));
+        // 오늘 날짜의 맨 마지막 시간 (23시 59분 59초)
+        LocalDateTime endOfDay = now.toLocalDate().atTime(LocalTime.MAX);
+
+        return Duration.between(now, endOfDay).getSeconds();
+    }
 
     @GetMapping("/delete/{id}") // 경로 변수(PathVariable) 방식 추천
     public String delete(@PathVariable Long id, RedirectAttributes rattr) {
---
+++ src/test/java/com/minji/hi_erp/NoticeViewTest.java
@@ -0,0 +1,75 @@
+package com.minji.hi_erp;
+
+import com.minji.hi_erp.entity.Notice;
+import com.minji.hi_erp.repository.NoticeRepository;
+import com.minji.hi_erp.service.NoticeService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+public class NoticeViewTest {
+
+    @Autowired
+    private NoticeService noticeService;
+
+    @Autowired
+    private NoticeRepository noticeRepository;
+
+    @Test
+    @DisplayName("2명의 유저가 동시에 게시글을 조회하면 조회수가 2만큼 증가해야 한다")
+    void increasViewCountTest() throws InterruptedException {
+        // 게시글 데이터 생성
+        Notice notice = new Notice("동시성 테스트 게시글","테스트 내용입니다.",null);
+
+        Notice savedNotice = noticeRepository.save(notice);
+        Long noticeId = savedNotice.getId();
+
+
+        // 동시 요청을 시뮬레이션 하기 위한 설정
+        int threadCount = 2;
+        ExecutorService executorService = Executors.newFixedThreadPool(threadCount);
+
+        CountDownLatch readyLatch = new CountDownLatch(threadCount);
+        CountDownLatch startLatch = new CountDownLatch(1);
+        CountDownLatch doneLatch = new CountDownLatch(threadCount);
+
+        // 2개의 스레드(유저)가 동시에 조회수 증가 메서드를 호출하도록 실행
+        for (int i=0; i < threadCount; i++) {
+            executorService.execute(() -> {
+                try {
+                    readyLatch.countDown();
+                    startLatch.await();
+
+                    //noticeService.increaseViewCount(noticeId); 기존 ++ 메서드
+                    noticeService.increasViewCountatDB(noticeId);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    doneLatch.countDown();
+                }
+            });
+        }
+
+        readyLatch.await();
+        startLatch.countDown(); // 🔥 출발 신호
+        doneLatch.await();
+
+        // 결과 검증
+        Notice resultNotice = noticeRepository.findById(noticeId)
+                .orElseThrow(() -> new IllegalArgumentException("게시글이 존재하지 않습니다."));
+
+        System.out.println("=====================================");
+        System.out.println("최종 조회수 결과: " + resultNotice.getViewCount());
+        System.out.println("=====================================");
+
+        assertThat(resultNotice.getViewCount()).isEqualTo(2);
+    }
+}
