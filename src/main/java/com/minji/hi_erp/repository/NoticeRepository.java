d4d0be68af9fdc81204834f52d08fe1b5cf296c2
minji856 <sistar96@gmail.com>
2026-06-24 17:29:08 +0900

    ✨ Feat : JPQL 벌크 연산 방식으로 변경 조회수 UPDATE 쿼리 추가

--- src/main/java/com/minji/hi_erp/repository/NoticeRepository.java
+++ src/main/java/com/minji/hi_erp/repository/NoticeRepository.java
@@ -4,6 +4,9 @@ import com.minji.hi_erp.entity.Notice;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -18,4 +21,8 @@ public interface NoticeRepository extends JpaRepository<Notice, Long> {
 
     // 가장 최신 5개글만
     List<Notice> findTop5ByOrderByCreatedDateDesc();
+
+    @Modifying(clearAutomatically = true)
+    @Query("UPDATE Notice n SET n.viewCount = :viewCount WHERE n.id = :noticeId")
+    void updateViewCount(@Param("noticeId") Long noticeId, @Param("viewCount") int viewCount);
 }
